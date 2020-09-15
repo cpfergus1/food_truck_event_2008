007895d9614cf668a0dcb66e9de404f6c98d9027
Connor Ferguson <cpfergus1@gmail.com>
2020-09-15 13:14:00 -0600

    Initialized item class

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,12 +4,12 @@ require './lib/item'
 
 class ItemTest <Minitest::Test
   def setup
-  item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-  item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_item_has_attributes
-    assert_equal 'Apple Pie (Slice)', item2.name
-    assert_equal '2.50', item2.price
+    assert_equal 'Apple Pie (Slice)', @item2.name
+    assert_equal '$2.50', @item2.price
   end
 end
